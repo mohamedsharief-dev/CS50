783c26f772055b24fccb5900d16dab00a426a3b9
kali <hello@moe-sharief.dev>
2024-03-22 19:32:00 -0500

    New Python file for practice

---
+++ lists.py
@@ -0,0 +1,8 @@
+# define a list of names
+names = ["Moe", "Chriss", "June"]
+
+print(names[0])
+
+names.append("Draco")
+
+print(names)
---
+++ sets.py
@@ -0,0 +1,10 @@
+# sets overview
+
+s = set()
+
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+
+print(s)
