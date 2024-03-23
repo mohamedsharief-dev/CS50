762a971fecadcc14973b8926d6416dc1da45fb60
kali <hello@moe-sharief.dev>
2024-03-23 18:34:12 -0500

    exceptions practice file

--- python/exceptions.py
+++ python/exceptions.py
@@ -1,6 +1,9 @@
-try:
-    x = int(input("What's x ?"))
-except ValueError:
-    print("You entered a non int")
-else:
-    print(f"x is {x}")
+while True:
+    try:
+        x = int(input("What's x ?; "))
+    except ValueError:
+        print("You Entered a Non Integer :(")
+    else:
+        break
+
+print(f"x is {x}")
