4422dfc41f4d763ba6fc3fdf7625dbadd84ff1e6
Mohamed Sharief <hello@moe-sharief.dev>
2024-03-23 11:14:22 +0300

    Dictionary Practice file

---
+++ python/hogwarts.py
@@ -0,0 +1,8 @@
+students = [
+    {"name": "Hermoine", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russe; terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronus": None}
+]
+for student in students:
+    print(student["name"], student["patronus"])
