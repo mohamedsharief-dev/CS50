03f7363c40e95395934a6a57f7fb6cbcea2759eb
Mohamed Sharief <hello@moe-sharief.dev>
2024-03-25 00:59:17 +0300

    deleted:    hello.html 	deleted:    lecture3/db.sqlite3 	deleted:    lecture3/hello/__init__.py 	deleted:    lecture3/hello/__pycache__/__init__.cpython-311.pyc 	deleted:  	deleted:    lecture3/hello/__pycache__/views.cpython-311.pyc deleted:    lecture3/hello/admin.pyw#deleted:    lecture3/hello/apps.pyd 	deleted:    lecture3/hello/migrations/__init__.py deleted:    lecture3/hello/models.py deleted:    lecture3/hello/tests.py deleted:    lecture3/hello/urls.py deleted:    lecture3/hello/views.py deleted:    lecture3/lecture3/__init__.py deleted:    lecture3/lecture3/__pycache__/__init__.cpython-311.pyc deleted:    lecture3/lecture3/__pycache__/settings.cpython-311.pyc deleted:    lecture3/lecture3/__pycache__/urls.cpython-311.pyc deleted:    lecture3/lecture3/__pycache__/wsgi.cpython-311.pyc deleted:    lecture3/lecture3/asgi.py deleted:    lecture3/lecture3/settings.py deleted:    lecture3/lecture3/urls.py deleted:    lecture3/lecture3/wsgi.py deleted:    lecture3/manage.py

--- lecture3/hello/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("moe", views.moe, name="moe"),
-    path("tayseer", views.tayseer, name="tayseer")
-]
