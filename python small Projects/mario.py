22291c619e2b4b19a4cf1fae538e4c0c9a2cda25
kali <hello@moe-sharief.dev>
2024-03-25 14:27:04 -0500

    Python debugging class

--- python small Projects/mario.py
+++ python small Projects/mario.py
@@ -1,2 +1,12 @@
-for _ in range(3):
-    print("#")
+def main():
+    height = int(input("Height: "))
+    pyramid(height)
+
+
+def pyramid(n):
+    for i in range(n):
+        print("#" * i)
+
+
+if __name__ == "__main__":
+    main()
