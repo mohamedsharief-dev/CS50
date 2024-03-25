26e769789e9d6076b68ffd0659844bb676951c5e
Mohamed Sharief <hello@moe-sharief.dev>
2024-03-25 18:35:45 +0300

    practice in c

--- projectsbuiltwithC/hello.c
+++ projectsbuiltwithC/hello.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-    printf("Hello, World! \n");
+    string answer = get_string("What's Your Name? ");
+    printf("Hello, %s \n", answer);
+
 }
